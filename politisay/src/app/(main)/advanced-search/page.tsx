47528ec83117d22fff9bddd8cf5b094aeb8e2877
Djtu75 <Gideonzim@gmail.com>
2025-06-23 15:49:03 -0500

    Basic Bar

--- politisay/src/app/(main)/advanced-search/page.tsx
+++ politisay/src/app/(main)/advanced-search/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Search from "@/app/search";
 
 export default function HomePage() {
   const handleClick = () => {
@@ -8,12 +9,9 @@ export default function HomePage() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="text-3xl font-bold mb-6">Welcome to the search Page</h1>
-      <button
-        onClick={() => handleClick()}
-        className="px-6 py-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-      >
-        Click Me
-      </button>
+      <div className="flex justify-center items-center ">
+        <Search placeholder="Quote Here"></Search>
+      </div>
     </main>
   );
 }
--- politisay/src/app/(main)/page.tsx
+++ politisay/src/app/(main)/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+import Search from "../search";
 
 export default function HomePage() {
   const handleClick = () => {
@@ -6,15 +7,23 @@ export default function HomePage() {
   };
 
   return (
-    <main className="flex flex-col items-center justify-center min-h-screen">
+    <main className="flex flex-col items-center justify-center">
       <h1 className="text-3xl font-bold mb-4">Welcome to PolitiSay!</h1>
-      <h3 className="text-2x1 mb-2"> Type in VVVV to search for a specific quote!</h3>
-      <button
-        onClick={() => handleClick()}
-        className="px-6 py-2 text-lg bg-blue-500 text-white rounded hover:bg-blue-600 transition"
-      >
-        Click Me
-      </button>
+      <div className="flex justify-center" id="search-bar">
+        <Search placeholder="Search Here"></Search>
+      </div>
+      <div id="results">
+        <ul>
+          <li>Hey</li>
+          <li>quote</li>
+          <li>sup</li>
+        </ul>
+      </div>
+      <div id="trending">
+        <h2 className="font-bold py-[25px]">Trending Quotes:</h2>
+
+      </div>
+      
     </main>
   );
 }
